9754781776761011d017077a92c4f1f88e2c3f0f
ketchupanezz <cheezsauce@mail.ru>
2025-04-28 14:16:38 +0300

    fix: remove vitest config from eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,6 @@ export default tseslint.config(
   {
     extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
     files: ['**/*.{ts,tsx}'],
-    ignores: ['vitest.config.ts'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
