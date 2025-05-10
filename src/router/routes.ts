a4dacf2d2b769caeb3d1b12e6b5a4027442bae2a
Elidion-netizen <sapegaserg@gmail.com>
2025-05-10 14:04:42 +0300

    fix: add data mode router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,12 +3,16 @@ import { createBrowserRouter } from 'react-router';
 import NotFoundPage from '@/pages/NotFoundPage';
 import LoginPage from '@/pages/LoginPage';
 import { protectedLoader } from './protected-router';
+import MainPage from '@/pages/MainPage';
+import RegistrationPage from '@/pages/RegistrationPage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     Component: RootPage,
     children: [
+      { index: true, Component: MainPage },
+      { path: 'register', Component: RegistrationPage },
       { path: 'login', loader: protectedLoader, Component: LoginPage },
     ],
   },
