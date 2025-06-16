3793aecc08f48bd7b771ec1609c621de9ede02fe
ketchupanezz <cheezsauce@mail.ru>
2025-06-17 00:21:07 +0300

    feat: implement about us page and update main page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,7 @@ import LoginPage from '@/pages/LoginPage';
 import { protectedLoader } from './protected-router';
 import MainPage from '@/pages/MainPage';
 import RegistrationPage from '@/pages/RegistrationPage';
-
+import AboutUsPage from '@/pages/AboutUsPage';
 export const router = createBrowserRouter([
   {
     path: '/',
@@ -18,6 +18,7 @@ export const router = createBrowserRouter([
       { path: 'main', Component: MainPage },
       { path: 'register', Component: RegistrationPage },
       { path: 'login', loader: protectedLoader, Component: LoginPage },
+      { path: 'about', Component: AboutUsPage },
     ],
   },
   {
