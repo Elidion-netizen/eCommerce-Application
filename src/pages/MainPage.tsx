a5a522e1a88ac1fc91adb7fc276cb408ef5524b2
ketchupanezz <126332050+ketchupanezz@users.noreply.github.com>
2025-06-18 00:27:55 +0300

    fix: img problem

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -156,7 +156,7 @@ const MainPage = (): React.JSX.Element => {
               order={{ base: 2, md: 1 }}
             >
               <Image
-                src="./src/components/ui/img/CrouSale.png"
+                src="https://i.postimg.cc/505ZkDB1/CrouSale.png"
                 alt="Sale"
                 w="500px"
                 h="auto"
